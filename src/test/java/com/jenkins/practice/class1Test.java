0161efa6f027d6279e9e199d7d4ee7ab1b758840
mohankumargowdaa <india.mohankumar@gmail.com>
2022-04-14 12:30:29 +0530

    first commit

---
+++ src/test/java/com/jenkins/practice/class1Test.java
@@ -0,0 +1,12 @@
+package com.jenkins.practice;
+
+import org.testng.annotations.Test;
+
+public class class1Test {
+	
+	@Test
+	public void testStep1() {
+		System.out.println("Step1 in class1");
+	}
+
+}
