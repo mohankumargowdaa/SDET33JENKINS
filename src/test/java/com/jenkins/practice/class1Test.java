9976ec6f0948a37e6abdadd180e42b295ffcc59a
mohangowdak <103551008+mohangowdak@users.noreply.github.com>
2022-05-31 17:39:23 +0530

    Update class1Test.java

--- src/test/java/com/jenkins/practice/class1Test.java
+++ src/test/java/com/jenkins/practice/class1Test.java
@@ -3,7 +3,7 @@ package com.jenkins.practice;
 import org.testng.annotations.Test;
 
 public class class1Test {
-	
+	//asdfghi
 	@Test
 	public void testStep1() {
 		System.out.println("Step1 in class1");
