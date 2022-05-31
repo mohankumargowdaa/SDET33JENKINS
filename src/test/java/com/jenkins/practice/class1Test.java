67bc4003b810d947ae38d404b94ac6bd1a166517
mohankumargowdaa <india.mohankumar@gmail.com>
2022-05-31 17:40:48 +0530

    asdfghi updated

--- src/test/java/com/jenkins/practice/class1Test.java
+++ src/test/java/com/jenkins/practice/class1Test.java
@@ -3,14 +3,15 @@ package com.jenkins.practice;
 import org.testng.annotations.Test;
 
 public class class1Test {
-	
+
 	@Test
 	public void testStep1() {
 		System.out.println("Step1 in class1");
 		System.out.println("Step1 edited in class1");
-		rkhkldjlkajdlkjlasjdlkfjalksdjlkjaslkjflasdj
+		//rkhkldjlkajdlkjlasjdlkfjalksdjlkjaslkjflasdj
 		//comment
 		//comment
 	}
 
 }
+//asdfghi
